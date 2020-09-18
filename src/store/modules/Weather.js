aa6c162b2e107238cbbbdb49c061df5752e1a1e2
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2020-09-18 14:00:27 +0200

    Make request with lat & lng on created

--- src/store/modules/Weather.js
+++ src/store/modules/Weather.js
@@ -1,4 +1,5 @@
-// import axios from 'axios';
+import axios from 'axios';
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
 const state = {
   currentCityTemp: 'hello',
@@ -8,7 +9,20 @@ const getters = {
   currentCityTemp: state => state.currentCityTemp,
 };
 
-const actions = {};
+const actions = {
+  async fetchWeather({ commit }, payload) {
+    const { city, lon, lat } = payload;
+    const response = await axios.get(`${baseUrl}/weather`, {
+      params: {
+        q: city || null,
+        appid: '641d47bc20e1dabe6a69d811be1a11af',
+        lon: lon || null,
+        lat: lat || null,
+      },
+    });
+    commit('setCityTemp', response.data);
+  },
+};
 
 const mutations = {};
 
